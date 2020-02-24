58f66bf4c18b403d5e5938374a100c250e3ef4c0
Daniel Szczepanik <tmlm18@gmail.com>
2020-02-24 09:32:16 +0100

    Render products index action using jsonapi

--- lib/fast_food_backend_web/controllers/product_controller.ex
+++ lib/fast_food_backend_web/controllers/product_controller.ex
@@ -1,43 +1,44 @@
 defmodule FastFoodBackendWeb.ProductController do
   use FastFoodBackendWeb, :controller
-
+  
   alias FastFoodBackend.Products
   alias FastFoodBackend.Products.Product
-
+  alias FastFoodBackendWeb.ProductView
+  
   action_fallback FastFoodBackendWeb.FallbackController
 
   def index(conn, _params) do
     products = Products.list_products()
-    render(conn, "index.json", products: products)
+    render(conn, ProductView, "index.json", %{data: products})
   end
 
-  def create(conn, %{"product" => product_params}) do
-    with {:ok, %Product{} = product} <- Products.create_product(product_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", Routes.product_path(conn, :show, product))
-      |> render("show.json", product: product)
-    end
-  end
+  # def create(conn, %{"product" => product_params}) do
+  #   with {:ok, %Product{} = product} <- Products.create_product(product_params) do
+  #     conn
+  #     |> put_status(:created)
+  #     |> put_resp_header("location", Routes.product_path(conn, :show, product))
+  #     |> render("show.json", product: product)
+  #   end
+  # end
 
-  def show(conn, %{"id" => id}) do
-    product = Products.get_product!(id)
-    render(conn, "show.json", product: product)
-  end
+  # def show(conn, %{"id" => id}) do
+  #   product = Products.get_product!(id)
+  #   render(conn, "show.json", product: product)
+  # end
 
-  def update(conn, %{"id" => id, "product" => product_params}) do
-    product = Products.get_product!(id)
+  # def update(conn, %{"id" => id, "product" => product_params}) do
+  #   product = Products.get_product!(id)
 
-    with {:ok, %Product{} = product} <- Products.update_product(product, product_params) do
-      render(conn, "show.json", product: product)
-    end
-  end
+  #   with {:ok, %Product{} = product} <- Products.update_product(product, product_params) do
+  #     render(conn, "show.json", product: product)
+  #   end
+  # end
 
-  def delete(conn, %{"id" => id}) do
-    product = Products.get_product!(id)
+  # def delete(conn, %{"id" => id}) do
+  #   product = Products.get_product!(id)
 
-    with {:ok, %Product{}} <- Products.delete_product(product) do
-      send_resp(conn, :no_content, "")
-    end
-  end
+  #   with {:ok, %Product{}} <- Products.delete_product(product) do
+  #     send_resp(conn, :no_content, "")
+  #   end
+  # end
 end
